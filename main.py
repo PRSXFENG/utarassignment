2940809bf802dffc8a6593cc801eeeffb266d87c
PRSXFENG <38828806+PRSXFENG@users.noreply.github.com>
2023-03-09 05:05:02 +0000

    menu1

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-mainloop = True
-while mainloop:
+run = True
+def mainloop():
     print ("-"*64)
     print ("Welcome to the Test Management System")
     print ("-"*64)
@@ -8,16 +8,6 @@ while mainloop:
     print ("[2] Generate Test Paper")
     print ("[3] Help")
     print ("\n[E] Exit")
-
-    def setupmenu():
-        print ("todo")
-
-    def testpaper():
-        print ("todo")
-
-    def helpmenu():
-        print ("todo")
-
     select = input()
 
     if select == "1":
@@ -29,9 +19,60 @@ while mainloop:
     elif select == "3":
         helpmenu()
 
-    elif select == "E":
+    elif select == "E" or select == "e":
         print ("Goodbye")
-        mainloop = False
+        exit()
 
     else:
         print ("Error: Invalid Input\nPlease Try Again.")
+
+def setupmenu():
+    print ("-"*64)
+    print ("Setup Menu")
+    print ("-"*64)
+    print ("Please pick an option: ")
+    print ("[1] Student Menu")
+    print ("[2] Courses Menu")
+    print ("[3] Question Banks")
+    print ("[4] Test Dates")
+    print ("\n[E] Exit")
+    select2 = input()
+
+    if select2 == "1":
+        studentmenu()
+
+    elif select2 == "2":
+        coursemenu()
+
+    elif select2 == "3":
+        questionbank()
+
+    elif select2 == "4":
+        testdate()
+
+    elif select2 == "E" or select2 == "e":
+        mainloop()
+    
+    else:
+        print ("Error: Invalid Input\nPlease Try Again.")
+
+def testpaper():
+    print ("todo")
+
+def helpmenu():
+    print ("todo")
+
+def studentmenu():
+    print ("todo")
+
+def coursemenu():
+    print ("todo")
+
+def questionbank():
+    print ("todo")
+
+def testdate():
+    print ("todo")
+
+while run:
+    mainloop()
