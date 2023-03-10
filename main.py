443cd48f104c0ce7d9d3a7ad3a275c2861743631
yucodings <127484198+yucodings@users.noreply.github.com>
2023-03-10 19:02:12 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 run = True
 
-
+#mainpage
 def mainloop():
     print("-" * 64)
     print("Welcome to the Test Management System")
@@ -28,7 +28,7 @@ def mainloop():
     else:
         print("Error: Invalid Input\nPlease Try Again.")
 
-
+#mainpage[1]
 def setupmenu():
     print("-" * 64)
     print("Setup Menu")
@@ -59,12 +59,14 @@ def setupmenu():
     else:
         print("Error: Invalid Input\nPlease Try Again.")
 
-
+#mainpage[2]
 def testpaper():
     print("-" * 64)
     print("Generate Student Test Paper")
-    print(
-        "there is supposed to be a list of courses here from another menu but for now it will just be this line of text")
+    print("-" * 64)
+ 
+    print(courseLst)
+    #there is supposed to be a list of courses here from another menu but for now it will just be this line of text
     courseid = input("Enter Course ID or E to Exit: ")
 
     if courseid == "E" or "e":
@@ -73,26 +75,50 @@ def testpaper():
     else:
         print("itll do something")
 
-
+#mainpage[3]
 def helpmenu():
+    print("-" * 64)
+    print("Help Menu")
+    print("-" * 64)
+    #here need to print the use of help menu, no idea
     print("todo")
 
-
+#setup[1]    
 def studentmenu():
+    studentlist()
+    #need to add name, add id, add nric
     print("todo")
-
-
+    
+#setup[2]
 def coursemenu():
     print("todo")
 
-
+#setup[3]
 def questionbank():
     print("todo")
 
-
+#setup[4]
 def testdate():
     print("todo")
 
+#Student List
+def studentlist():
+    print("-" * 64)
+    print("Student List")
+    print("-" * 64)
+    print("Student ID"," "*12,"Student Name"," "*27,"NRIC Number")
+    print("-" * 64)
+    #need to list from up to down(not sure)
+    for i in range (len(stuId)):
+        print(studId," "*3,studName," "*23,nric)
+    print("-" * 64)
+
+
+courseCode=[]
+courseDes=[]
+studId=[]
+studName=[]
+nric=[]
 
 while run:
     mainloop()
